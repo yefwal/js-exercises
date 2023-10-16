47402e98227ed1b5ad231665f969d16ab99dffaf
yefwal <145699390+yefwal@users.noreply.github.com>
2023-10-16 06:24:13 -0700

    - can re-visit all these problems on codewars

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (str, num) {
+  let string = "";
+  if (num < 0) {
+    return "ERROR";
+  }
+  for (let i = 0; i < num; i++) {
+    string += str;
+  }
+  return string;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function (str) {
+  let box = [];
+  let newStr = str.split("");
+  for (let i = 0; i < newStr.length; i++) {
+    box.push(newStr[i]);
+  }
 
+  return box.reverse().join("");
 };
 
 // Do not edit below this line
